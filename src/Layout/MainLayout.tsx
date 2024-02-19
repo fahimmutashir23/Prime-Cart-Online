693526f0a0be3e2d8b297ea99da9ea5563098b46
fahimmutashir23 <taslimit23@gmail.com>
2024-02-19 17:27:30 +0600

    added nav and footer layout

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../Components/Shared/Navbar/Navbar";
+import Footer from "../Components/Shared/Footer/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
