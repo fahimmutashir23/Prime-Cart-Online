1228e269a1541a4b03076536cc8bd773f25024ae
fahimmutashir23 <taslimit23@gmail.com>
2024-02-19 17:07:43 +0600

    initial setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import MainLayout from './Layout/MainLayout';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<MainLayout />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-red-800">Home</h1>
+        </div>
+    );
+};
+
+export default Home;
